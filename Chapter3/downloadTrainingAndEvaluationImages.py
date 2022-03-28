d3fe9ab20db69f029811ab9c9cd977cde1797cc0
gctziavas <gctziavas@gmail.com>
2022-03-28 13:41:55 +0300

    chapter 3 micro fixes

--- Chapter3/downloadTrainingAndEvaluationImages.py
+++ Chapter3/downloadTrainingAndEvaluationImages.py
@@ -11,3 +11,11 @@ if __name__ == '__main__':
     zip_ref = zipfile.ZipFile(file_name, 'r')
     zip_ref.extractall(training_dir)
     zip_ref.close()
+
+    validation_url = "https://storage.googleapis.com/laurencemoroney-blog.appspot.com/validation-horse-or-human.zip"
+    validation_file_name = "validation-horse-or-human.zip"
+    validation_dir = 'horse-or-human/validation/'
+    urllib.request.urlretrieve(validation_url, validation_file_name)
+    zip_ref = zipfile.ZipFile(validation_file_name, 'r')
+    zip_ref.extractall(validation_dir)
+    zip_ref.close()
--- Chapter3/main.py
+++ Chapter3/main.py
@@ -1,12 +1,62 @@
 # This is a sample Python script.
 
+import os
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import shutil
+import urllib.request
+import urllib.request
+import zipfile
 
 import tensorflow as tf
 
 
-#Implementing Convolutional Neural Networks
+# Downloads the pictures used in the CNN
+def human_or_horse_images():
+    url = "https://storage.googleapis.com/laurencemoroney-blog.appspot.com/horse-or-human.zip"
+    file_name = "horse-or-human.zip"
+    training_dir = 'horse-or-human/training/'
+    urllib.request.urlretrieve(url, file_name)
+    zip_ref = zipfile.ZipFile(file_name, 'r')
+    zip_ref.extractall(training_dir)
+    zip_ref.close()
+
+    # File name
+    file = 'horse-or-human.zip'
+    # File location
+    location = "/home/comlab/Projects/MLForCoders/Chapter3/"
+    # Path
+    path = os.path.join(location, file)
+    # Remove the file 'horse-or-human.zip'
+    os.remove(path)
+
+    validation_url = "https://storage.googleapis.com/laurencemoroney-blog.appspot.com/validation-horse-or-human.zip"
+    validation_file_name = "validation-horse-or-human.zip"
+    validation_dir = 'horse-or-human/validation/'
+    urllib.request.urlretrieve(validation_url, validation_file_name)
+    zip_ref = zipfile.ZipFile(validation_file_name, 'r')
+    zip_ref.extractall(validation_dir)
+    zip_ref.close()
+
+    # File name
+    file = 'validation-horse-or-human.zip'
+    # File location
+    location = "/home/comlab/Projects/MLForCoders/Chapter3/"
+    # Path
+    path = os.path.join(location, file)
+    # Remove the file 'horse-or-human.zip'
+    os.remove(path)
+
+
+def delete_images():
+    location = "/home/comlab/Projects/MLForCoders/Chapter3/"
+    directory = "horse-or-human"
+    path = os.path.join(location, directory)
+    # removing directory
+    shutil.rmtree(path)
+
+
+# Implementing Convolutional Neural Networks
 def examplePage37():
     data = tf.keras.datasets.fashion_mnist
     (training_images, training_labels), (test_images, test_labels) = data.load_data()
@@ -33,7 +83,11 @@ def examplePage37():
     print(classifications[0])
     print(test_labels[0])
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    human_or_horse_images()
+
     examplePage37()
 
+    delete_images()
